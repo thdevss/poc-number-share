018c37ed28ace691e454952e36474963ca32e361
thdevss <pichet.suesat@gmail.com>
2024-02-15 18:21:48 +0700

    fix schema COLLATE

--- schema.sql
+++ schema.sql
@@ -25,7 +25,7 @@ CREATE TABLE `number_share` (
   `error` text,
   `status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 
